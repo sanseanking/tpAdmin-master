8841688ef0333a064e0940c0d3e2d0465a59b593
sanseanking <sansean@live.cn>
2018-12-21 20:00:23 +0800

    111

---
+++ tpadmin.sql
@@ -0,0 +1,21 @@
+
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 50553
+ Source Host           : localhost:3306
+ Source Schema         : tpadmin
+
+ Target Server Type    : MySQL
+ Target Server Version : 50553
+ File Encoding         : 65001
+
+ Date: 20/12/2018 18:18:49
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET FOREIGN_KEY_CHECKS = 1;
